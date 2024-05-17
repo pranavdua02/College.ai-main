405272d570e93a1072b066501c98b98b6373af96
Srs_vesta <srslinkedin89@gmail.com>
2024-05-17 22:16:55 +0530

    Update about

--- pages/10_❕About.py
+++ pages/10_❕About.py
@@ -37,14 +37,14 @@ st.write("\n")
 st.header("Page info:")
 
 def Lens():
-    st.markdown("2. AI Lens")
+    st.markdown("1. AI Lens")
     with open('src/AI Lens.json') as anim_source:
         animation = json.load(anim_source)
         st_lottie(animation, 1, True, True, "high", 100, -200)
     st.write("This will allow us to read images by AI and Give us responce on it,\n AI Vision Functionality \n ChatBot is usefull for Text Query input processing.")
 
 def Ask_To_PDF():
-    st.markdown("1. Ask_To_PDF")
+    st.markdown("2. Ask_To_PDF")
     with open('src/pdf.json') as anim_source:
         animation = json.load(anim_source)
         st_lottie(animation, 1, True, True, "high", 100, -200)
@@ -52,14 +52,14 @@ def Ask_To_PDF():
     st.write("This service provides you the functionality to train the AI_Generative model.\n on your PDF and then apply your query on it.")
 
 def ATS():
-    st.markdown("2. ATS")
+    st.markdown("3. ATS")
     with open('src/ATS.json') as anim_source:
         animation = json.load(anim_source)
         st_lottie(animation, 1, True, True, "high", 100, -200)
     st.write("Check if your resume is suitable for the job or not,\n Check if the job is good for you or not, \n Get recommendations based on your resume and job description.")
 
 def ResumeAnalyzer():
-    st.markdown("3. ResumeAnalyzer")
+    st.markdown("4. ResumeAnalyzer")
     with open('src/Resume.json', 'r', encoding='utf-8') as anim_source:
         animation = json.load(anim_source)
         st_lottie(animation, 1, True, True, "high", 100, -200)
