5d2aa40b93b67d0622dacb516ec23bcecfa68388
Srs_vesta <srslinkedin89@gmail.com>
2024-05-18 09:32:42 +0530

    Update project to version 2.0 with new features and improvements

---
+++ backend/server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('message')
+def handleMessage(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
