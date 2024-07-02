693ad9ad2e4ffbab73c37718013a633c6a9d974a
Srs_vesta <101057653+SurajSanap@users.noreply.github.com>
2024-07-02 21:02:53 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -125,34 +125,36 @@ def main():
     menu[tabs]()
     
     # Footer
+    st.markdown("""
    footer="""<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
-
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: white;
-color: black;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p>Developed by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Suraj Sanap</a></p>
-</div>
-"""
-st.markdown(footer,unsafe_allow_html=True)
+    a:link , a:visited{
+    color: blue;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+    
+    a:hover,  a:active {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+    }
+    
+    .footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    color: black;
+    text-align: center;
+    }
+    </style>
+    <div class="footer">
+    <p>Developed by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Suraj Sanap</a></p>
+    </div>
+    """
+    st.markdown(footer,unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
