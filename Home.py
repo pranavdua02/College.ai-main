182e46ed2b0f24386ca99630e79cd389c9a9728f
Srs_vesta <101057653+SurajSanap@users.noreply.github.com>
2024-07-02 21:07:56 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -125,36 +125,10 @@ def main():
     menu[tabs]()
     
     # Footer
-    footer="""<style>
-a:link , a:visited{
-color: blue;
-background-color: transparent;
-text-decoration: underline;
-}
+   temperature = "-10"
 
-a:hover,  a:active {
-color: red;
-background-color: transparent;
-text-decoration: underline;
-}
-
-.footer {
-position: fixed;
-left: 0;
-bottom: 0;
-width: 100%;
-background-color: white;
-color: black;
-text-align: center;
-}
-</style>
-<div class="footer">
-<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
-</div>
-"""
-st.markdown(footer,unsafe_allow_html=True)
-
-Hello @Heflin_Stephen_Raj_S , this worke
+    st.write(f"SurajSanap2024 :blue[{temperature}]")
+    
 
 if __name__ == "__main__":
     main()
